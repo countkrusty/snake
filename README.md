6048b45930af4f74ce8559525c803d37370b91ff
countkrusty <tristan.wiessalla@posteo.de>
2019-06-28 10:58:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#SNAKE game
+
+This is to get to know basic functions of the ncurses library. 
+Missing features:
+
+1) End game if you bite your own tail
+2) smarter management of tail bits
+3) Add a menu
---
+++ main.cpp
@@ -0,0 +1,155 @@
+//
+//  main.cpp
+//  SnakeC++
+//
+//  Created by Tristan Wießalla on 10.06.19.
+//  Copyright © 2019 Tristan Wießalla. All rights reserved.
+//
+
+#include <iostream>
+#include <ncurses.h> //I/O library, not supported for Windows, alternative: conio 
+#include <unistd.h> //for sleep functions
+#include <cstdlib> //for rand function
+using namespace std;
+
+bool gameOver;
+
+const int width = 40; // Specify map dimensions
+const int heigth = 20;
+int x, y, fruitx, fruity, score; // head & fruit positions, score
+int tailx[100], taily[100];
+int nTail;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = heigth / 2;
+    srand(time(NULL));
+    fruitx = (rand() % width-1)+1;
+    fruity =(rand() % heigth-1)+1;
+    score = 0;
+}
+
+void Draw()
+{
+    initscr();
+    curs_set(0);
+    
+    for (int i = 0; i <= heigth+1; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            if (i == 0 || i == heigth+1)
+                mvaddstr(i, j, "#"); //Horizontal walls
+            if (i < heigth+1 && (j == 0 || j == width))
+                mvaddstr(i, j, "#"); //Vertical walls    for (int i = 0; i < width + 1; i++)
+        }
+    }
+    mvaddstr(fruity, fruitx, "*");
+    mvaddstr(y, x, "O");    
+    for(int i = 0; i < nTail; i++){
+	mvaddstr(taily[i], tailx[i], "o");
+	}
+    mvprintw(0, width+5, "score: %i", score);
+    
+}
+
+void Input()
+{
+nodelay(stdscr, TRUE); //program doesn't block until key is pressed but loops continuously 
+        switch(getch())
+        {
+            case  ' ':
+                break;
+            case  'a':
+                dir = LEFT;
+                break;
+            case  'd':
+                dir = RIGHT;
+                break;
+            case  'w':
+                dir = UP;
+                break;
+            case  's':
+                dir = DOWN;
+                break;
+            case  'x':
+                gameOver = true;
+                break;
+            default:
+                break;
+     }
+}
+
+void Logic()
+{
+    tailx[0] = x; //0th tail element = head
+    taily[0] = y;
+    int prevX = tailx[0];
+    int prevY = taily[0];
+    int prev2X, prev2Y;
+     
+    for(int i = 1; i < nTail; i++){
+	prev2X = tailx[i]; //the tail element moves to where the prior elem 
+	prev2Y = taily[i]; //ent was during last loop
+	tailx[i] = prevX; //tail element gets reset to head
+	taily[i] = prevY;
+	prevX = prev2X; //head gets reset to ???
+	prevY = prev2Y;
+	}
+    switch(int(dir))
+    {
+        case 1:
+            dir = LEFT;
+            x--; //'When go left, decrease x-dim value (x) by one
+	    break;
+        case 2:
+            dir = RIGHT;
+            x++;
+            break;
+        case 3:
+            dir = UP;
+            y--;
+            break;
+        case 4:
+            dir = DOWN;
+            y++;
+            break;
+        default:
+            break;
+    }
+    if(x >= width || x <= 0 || y >heigth || y <= 0) 
+	gameOver = true; 
+    
+    if(x == fruitx && y == fruity){
+        fruitx = rand() % width+1; fruity = rand() % heigth+1;
+        score += 5;
+        nTail++;
+    }
+}
+
+int main()
+{
+    Setup();
+    do // Loop all program elements unless gameOver has become true.
+    {
+        initscr();
+        erase();
+        noecho();
+        Draw();
+        Input();
+        Logic();
+        usleep(300000);
+        refresh();
+    }while (!gameOver);
+    endwin();
+    return 0;
+    
+}
+
+
